1219206a00d4c88272a65495b27f0659067f85ad
Tara Kaestner <tara.livebeautiful@gmail.com>
2017-09-20 19:55:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // write your code below!
-var name = "Susan"
-var height = 39
-var message = ` ${name} is ${height} inches tall`
+function sayHello() {console.log('Hello')
 
-module.exports = {name, height, message}
+}
+sayHello()
